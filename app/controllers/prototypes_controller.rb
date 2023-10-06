bad1b03696665727b49798eda0383aa38df4d8bd
maximumry <144888797+maximumry@users.noreply.github.com>
2023-10-06 19:58:17 +0900

    ddd

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @prototype = Prototype.all
