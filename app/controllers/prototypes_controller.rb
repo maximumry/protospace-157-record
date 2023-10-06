59b1401f8cfa4dee8f392ea886eb913736913d24
maximumry <144888797+maximumry@users.noreply.github.com>
2023-10-06 11:27:02 +0900

    createの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,26 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, expect: [:index, :show]
+  
   def index
     @prototype = Prototype.all
   end
 
   def new
-    @prototype = Prototype.new
+      @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def prototype_params
+    params.require(:prototype).permit(:proto_name, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
